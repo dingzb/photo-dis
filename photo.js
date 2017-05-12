28a7bcd1faf997d3618cf2ca677b2c6d29198e34
Neo <dzb9898@qq.com>
2017-05-12 18:14:26 +0800

    a

---
+++ photo.js
@@ -0,0 +1,8 @@
+var photos = {
+    '生活': ['ig/timg.jpg', 'ig/2.jpg', 'ig/3.jpg'],
+    '工作': ['ig/4.jpg', 'video/香肠派对BD1280高清中英双字.mp4', 'ig/6.jpg'],
+    '生活1': ['ig/1.jpg', 'ig/2.jpg', 'ig/3.jpg'],
+    '工作1': ['ig/4.jpg', 'ig/5.jpg', 'ig/6.jpg'],
+    '生活2': ['ig/1.jpg', 'ig/2.jpg', 'ig/3.jpg'],
+    '工作2': ['ig/4.jpg', 'ig/5.jpg', 'ig/6.jpg']
+};
