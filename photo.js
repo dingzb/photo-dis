75a52eab86c867b5d2512e597db5d3c3e689f01c
Neo <dzb9898@qq.com>
2017-05-15 16:41:30 +0800

    增加三层跳转，模块居中，图片描述

--- photo.js
+++ photo.js
@@ -1,8 +1,39 @@
-var photos = {
-    '生活': ['ig/timg.jpg', 'ig/2.jpg', 'ig/3.jpg'],
-    '工作风险安全防控': ['ig/4.jpg', 'video/香肠派对BD1280高清中英双字.mp4', 'ig/6.jpg'],
-    '生活1': ['ig/1.jpg', 'ig/2.jpg', 'ig/3.jpg'],
-    '工作1': ['ig/4.jpg', 'ig/5.jpg', 'ig/6.jpg'],
-    '生活2': ['ig/1.jpg', 'ig/2.jpg', 'ig/3.jpg'],
-    '工作2': ['ig/4.jpg', 'ig/5.jpg', 'ig/6.jpg']
-};
+var agencyPhotos = [{
+    '税源管理股': [{
+        '生活': [{ 'ig/timg.jpg': '开发会' }, { 'ig/2.jpg': '研讨会' }, { 'ig/3.jpg': '调研会' }],
+        '工作风险安全防控': [{ 'ig/4.jpg': 'aa' }, { 'video/香肠派对BD1280高清中英双字.mp4': 'bb' }, { 'ig/6.jpg': 'cc' }],
+        '生活1': [{ 'ig/1.jpg': 'dd' }, { 'ig/2.jpg': 'ee' }, { 'ig/3.jpg': 'ff' }]
+    }, {
+        '工作1': [{ 'ig/4.jpg': 'gg' }, { 'ig/5.jpg': 'hh' }, { 'ig/6.jpg': 'ii' }],
+        '生活2': [{ 'ig/1.jpg': 'jj' }, { 'ig/2.jpg': 'kk' }, { 'ig/3.jpg': 'll' }],
+        '工作2': [{ 'ig/4.jpg': 'mm' }, { 'ig/5.jpg': 'nn' }, { 'ig/6.jpg': 'oo' }]
+    }],
+    '马道头税务所': [{
+        '工作1': [{ 'ig/4.jpg': 'gg' }, { 'ig/5.jpg': 'hh' }, { 'ig/6.jpg': 'ii' }],
+        '生活2': [{ 'ig/1.jpg': 'jj' }, { 'ig/2.jpg': 'kk' }, { 'ig/3.jpg': 'll' }],
+        '工作2': [{ 'ig/4.jpg': 'mm' }, { 'ig/5.jpg': 'nn' }, { 'ig/6.jpg': 'oo' }]
+    }, {
+        '工作风险安全防控': [{ 'ig/4.jpg': 'aa' }, { 'video/香肠派对BD1280高清中英双字.mp4': 'bb' }, { 'ig/6.jpg': 'cc' }],
+        '生活2': [{ 'ig/1.jpg': 'jj' }, { 'ig/2.jpg': 'kk' }, { 'ig/3.jpg': 'll' }],
+        '工作2': [{ 'ig/4.jpg': 'mm' }, { 'ig/5.jpg': 'nn' }, { 'ig/6.jpg': 'oo' }]
+    }]
+}, {
+    '鹊山税务所': [{
+        '工作1': [{ 'ig/4.jpg': 'gg' }, { 'ig/5.jpg': 'hh' }, { 'ig/6.jpg': 'ii' }],
+        '生活2': [{ 'ig/1.jpg': 'jj' }, { 'ig/2.jpg': 'kk' }, { 'ig/3.jpg': 'll' }],
+        '工作2': [{ 'ig/4.jpg': 'mm' }, { 'ig/5.jpg': 'nn' }, { 'ig/6.jpg': 'oo' }]
+    }, {
+        '生活': [{ 'ig/timg.jpg': '开发会' }, { 'ig/2.jpg': '研讨会' }, { 'ig/3.jpg': '调研会' }],
+        '工作风险安全防控': [{ 'ig/4.jpg': 'aa' }, { 'video/香肠派对BD1280高清中英双字.mp4': 'bb' }, { 'ig/6.jpg': 'cc' }],
+        '生活1': [{ 'ig/1.jpg': 'dd' }, { 'ig/2.jpg': 'ee' }, { 'ig/3.jpg': 'ff' }]
+    }],
+    '稽查局': [{
+        '工作1': [{ 'ig/4.jpg': 'gg' }, { 'ig/5.jpg': 'hh' }, { 'ig/6.jpg': 'ii' }],
+        '生活2': [{ 'ig/1.jpg': 'jj' }, { 'ig/2.jpg': 'kk' }, { 'ig/3.jpg': 'll' }],
+        '工作2': [{ 'ig/4.jpg': 'mm' }, { 'ig/5.jpg': 'nn' }, { 'ig/6.jpg': 'oo' }]
+    }, {
+        '工作1': [{ 'ig/4.jpg': 'gg' }, { 'ig/5.jpg': 'hh' }, { 'ig/6.jpg': 'ii' }],
+        '生活2': [{ 'ig/1.jpg': 'jj' }, { 'ig/2.jpg': 'kk' }, { 'ig/3.jpg': 'll' }],
+        '工作2': [{ 'ig/4.jpg': 'mm' }, { 'ig/5.jpg': 'nn' }, { 'ig/6.jpg': 'oo' }]
+    }]
+}]
